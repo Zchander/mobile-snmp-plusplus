0d37d37c219ae0a40c90700a7b589d70def1c7b5
Xander Maas <xander@xjmaas.nl>
2012-10-30 15:47:02 +0100

    Initial Commit

---
+++ Mobile SNMP++/Mobile_SNMP__.h
@@ -0,0 +1,13 @@
+//
+//  Mobile_SNMP__.h
+//  Mobile SNMP++
+//
+//  Created by Xander Maas on 30-10-12.
+//  Copyright (c) 2012 Xander Maas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mobile_SNMP__ : NSObject
+
+@end
---
+++ Mobile SNMP++/Mobile_SNMP__.m
@@ -0,0 +1,13 @@
+//
+//  Mobile_SNMP__.m
+//  Mobile SNMP++
+//
+//  Created by Xander Maas on 30-10-12.
+//  Copyright (c) 2012 Xander Maas. All rights reserved.
+//
+
+#import "Mobile_SNMP__.h"
+
+@implementation Mobile_SNMP__
+
+@end
